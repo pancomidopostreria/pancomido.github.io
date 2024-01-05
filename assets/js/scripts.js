1ad2166452ab23c79139b7f10364312503ad7218
Pedro Maya <bernabemayapedrodejesus@gmail.com>
2024-01-05 00:56:10 -0600

    Arreglando menu dedsplegable

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
-const toggleMenuElement = document.getElementById('toggle-menu');
-const menuListElement = document.getElementById('menu__list');
 
-toggleMenuElement.addEventListener('click', () =>{
-    menuListElement.classList.toggle('menu__list--show');
-});
+  const toggleMenuElement = document.getElementById("toggle-menu");
+  const menuListElement = document.getElementById("menu__list");
+  
+    toggleMenuElement.addEventListener("click", () => {
+        menuListElement.classList.toggle("menu__list--show");
+      });
+
