6f48e3f4bf9366e4d268548e5757aa9aa0e2234a
Pedro Maya <bernabemayapedrodejesus@gmail.com>
2024-01-04 22:40:25 -0600

    Añadiendo toggle menu

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+const toggleMenuElement = document.getElementById('toggle-menu');
+const menuListElement = document.getElementById('menu__list');
+
+toggleMenuElement.addEventListener('click', () =>{
+    menuListElement.classList.toggle('menu__list--show');
+})
